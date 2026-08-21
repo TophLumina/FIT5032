f924be5e3e6ac10376f35d6d79f402891c15575e
Toph Lumina <2625792753@qq.com>
2026-08-21 20:01:21 +1000

    e2

--- Week2/tyang-library/src/App.vue
+++ Week2/tyang-library/src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
 import JSONLab from './components/JSON.vue'
+import Form from './components/LibraryRegistrationForm.vue'
+
 </script>
 
 <template>
-  <JSONLab />
+   <Form />
 </template>
-
-<style scoped></style>
--- Week2/tyang-library/src/main.js
+++ Week2/tyang-library/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// bootstrap css 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './style.css'
+
 createApp(App).mount('#app')
